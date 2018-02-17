56509a3acf2a998eac4cb39f0bf059837f3b0e86
Shane Moyo <smoyo@pdx.edu>
2018-02-17 12:40:27 -0800

    add apikey to process.env

--- src/App.js
+++ src/App.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const apiKey = process.env.REACT_APP_OMDB_API_KEY;
+console.log('apikey', apiKey)
+
 class App extends Component {
+
+  state = {
+    items: [],
+    loading: false,
+  }
+  
+  async loadResource(zip){
+    this.setState({ loading: true });
+    const url = `http://api.wunderground.com/api/${apiKey}/conditions/q/${zip}.json`;
+    console.log('url.................', url);
+    const response = await fetch(url);
+    let body = await response.json();
+    body.Error ? body = [] : body = body.Search;
+    console.log('responseeeeeeeeeee', body);
+    this.setState({
+      items: body,
+      loading: false
+    });
+  }
+
+  componentDidMount(){
+    this.loadResource('97008');
+  }
+
   render() {
     return (
       <div className="App">
