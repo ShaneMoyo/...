ab0bf6db080e1850d6a9cd1fa4a1b89c47c1efcc
Shane Moyo <smoyo@pdx.edu>
2018-02-19 14:09:25 -0800

    added temperatue difference

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,7 +5,13 @@ import '../../style/mystyle.css';
 class App extends Component {
 
   render() {
-    return <CurrentConditions/>;  
+    return (
+      <div class="container has-text-centered">
+        <h1 class="title has-text-grey">Weather Application</h1>
+        <hr/>
+        <CurrentConditions/>
+      </div>
+    );  
   }
 }
 
