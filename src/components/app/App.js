42eb49a316238a38b4e1363fe026cb767b2fe02e
Shane Moyo <smoyo@pdx.edu>
2018-02-20 16:40:21 -0800

    change background color

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,8 +6,8 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container has-text-centered">
-        <h1 className="title has-text-grey">Weather Application</h1>
+      <div className="container app has-text-centered">
+        <h1 className="title has-text-white">Weather Application</h1>
         <hr/>
         <CurrentConditions/>
       </div>
